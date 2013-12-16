f9ab475cbda2037074b599c9a96ef9565dc9f509
Graham Christensen <graham@grahamc.com>
2013-12-16 11:23:19 -0600

    Symlink dem bittie.

---
+++ RoboRooter/Component/Symlink.py
@@ -0,0 +1,58 @@
+
+
+import FileHint
+import os
+
+class Symlink(FileHint.FileHint):
+  def __init__(self):
+    self.hint_name = './manifests/symlinks'
+    self.state_expression = r'^(.+)\s+(.+)$'
+    self.rules = []
+
+  def _filter_violations(self, path):
+    for rule in self.rules:
+      link_src = self._relativize_link(rule[0], rule[1])
+      link_dest = os.path.join(path, rule[1])
+
+      if not self._is_path_linking_to_location(link_src, link_dest):
+        print("Expected %s to link to %s" % (link_src, link_dest))
+        yield (link_src, link_dest)
+
+  def _relativize_link(self, source, target):
+      src_dir = os.path.dirname(source)
+      src_file = os.path.basename(source)
+      target_dir = os.path.dirname(target)
+
+      relative_location = os.path.relpath(src_dir, target_dir)
+
+      link_src = os.path.join(relative_location, src_file)
+
+      return link_src
+
+
+  def _is_path_linking_to_location(self, source, target):
+    if not os.path.exists(target):
+      return False
+
+    if not os.path.islink(target):
+      return False
+
+    if os.readlink(target) != source:
+      return False
+
+    return True
+
+
+  def needs_fixing(self, path):
+    for path in self._filter_violations(path):
+      return True
+
+    return False
+
+  def fix(self, path):
+    for change in self._filter_violations(path):
+
+      os.symlink(change[0], change[1])
+
+
+
--- roborooter.py
+++ roborooter.py
@@ -7,25 +7,36 @@ from RoboRooter.Component.Owner import Owner
 from RoboRooter.Component.DeviceFile import DeviceFile
 from RoboRooter.Component.Content import Content
 from RoboRooter.Component.Whitelist import Whitelist
+from RoboRooter.Component.Symlink import Symlink
 
 
 config = ConfigLoader('./example/roborooter.ini')
 manifest_loader = ManifestLoader(config.get_config())
 manifest_loader.add_component(Content())
 manifest_loader.add_component(DeviceFile())
+manifest_loader.add_component(Symlink())
 manifest_loader.add_component(Permission())
 manifest_loader.add_component(Owner())
 manifest_loader.add_component(Whitelist())
 manifest = manifest_loader.get_manifest_by_version(1)
 
 path = './example/target/'
-for comp in manifest.components:
-  name = comp.__class__.__name__
 
-  needs_fixing = comp.needs_fixing(path)
-  print "%s: %s" % (name, needs_fixing)
+fixes = 1
+attempts = 0
+max_attempts = 1
+while fixes > 0 and attempts < 1:
+  fixes = 0
+  attempts += 1
+  for comp in manifest.components:
+    name = comp.__class__.__name__
 
-  if needs_fixing:
-    comp.fix(path)
+    needs_fixing = comp.needs_fixing(path)
+    print "%s: %s" % (name, needs_fixing)
+
+    if needs_fixing:
+      fixes += 1
+      comp.fix(path)
+  print "Made %d fixes on attempt %d" % (fixes, attempts)
 
 
