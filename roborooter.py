097725191b8b7f2eb3de779de651c3e44a02041e
Graham Christensen <graham@grahamc.com>
2013-12-16 09:59:27 -0600

    Setup block device component on the whitelist.

--- roborooter.py
+++ roborooter.py
@@ -4,17 +4,17 @@ from RoboRooter.ConfigLoader import ConfigLoader
 from RoboRooter.ManifestLoader import ManifestLoader
 from RoboRooter.Component.Permission import Permission
 from RoboRooter.Component.Owner import Owner
+from RoboRooter.Component.BlockDevice import BlockDevice
 from RoboRooter.Component.Content import Content
 from RoboRooter.Component.Whitelist import Whitelist
-import pprint
-import os
 
 
 config = ConfigLoader('./example/roborooter.ini')
 manifest_loader = ManifestLoader(config.get_config())
 manifest_loader.add_component(Content())
-manifest_loader.add_component(Owner())
+manifest_loader.add_component(BlockDevice())
 manifest_loader.add_component(Permission())
+manifest_loader.add_component(Owner())
 manifest_loader.add_component(Whitelist())
 manifest = manifest_loader.get_manifest_by_version(1)
 
